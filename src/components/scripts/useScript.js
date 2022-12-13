47e29ab94ea3451923c29c0fe42b8e2a9fc4497d
Umais <mianumais1997@gmail.com>
2022-12-13 09:49:03 +0500

    change useruffle file

--- src/components/scripts/useScript.js
+++ src/components/scripts/useScript.js
@@ -1,18 +1,19 @@
 import { useEffect } from 'react';
 
-const useScript = url => {
-    useEffect(() => {
-        const script = document.createElement('script');
+const useScript = (url, onLoad) => {
+  useEffect(() => {
+    const script = document.createElement('script');
 
-        script.src = url;
-        script.async = true;
+    script.src = url;
+    script.async = true;
 
-        document.body.appendChild(script);
+    document.body.appendChild(script);
+    script.addEventListener('load', onLoad);
 
-        return () => {
-            document.body.removeChild(script);
-        }
-    }, [url]);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [url]);
 };
 
 export default useScript;
